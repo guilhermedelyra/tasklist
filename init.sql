45284a68e91d740e5c354633f310447b9dd876f0
Guilherme de Lyra <guilyra12@gmail.com>
2021-09-11 05:03:26 -0300

    fixing user db /2

--- init.sql
+++ init.sql
@@ -1,3 +1,3 @@
-CREATE TABLE IF NOT EXISTS "user" (id varchar PRIMARY KEY, name varchar NOT NULL, email varchar UNIQUE NOT NULL, profile_pic varchar NOT NULL);
+CREATE TABLE IF NOT EXISTS "user" ("id" varchar PRIMARY KEY, "name" varchar NOT NULL, "email" varchar UNIQUE NOT NULL, "profile_pic" varchar NOT NULL);
 
-CREATE TABLE IF NOT EXISTS "tasks" (id SERIAL PRIMARY KEY, task varchar NOT NULL,   status varchar,   user_id varchar NOT NULL,   CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES "user"(id));
+CREATE TABLE IF NOT EXISTS "tasks" ("id" SERIAL PRIMARY KEY, "task" varchar NOT NULL, "status" varchar, "user_id" varchar NOT NULL, CONSTRAINT "fk_user" FOREIGN KEY("user_id") REFERENCES "user"("id"));
