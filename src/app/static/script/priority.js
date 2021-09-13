84e1cf991d05ec8aef716d45a357994d35dcc18f
Guilherme de Lyra <guilyra12@gmail.com>
2021-09-13 19:07:12 -0300

    restyling priority /4

--- src/app/static/script/priority.js
+++ src/app/static/script/priority.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
-    // example: https://getbootstrap.com/docs/4.2/components/modal/
-    // show modal
-    var text = parseFloat(document.querySelector('.priority').textContent);
-    console.log(text);
-    console.log(hsl_col_perc(text));
-
     function hsl_col_perc(percent, start=240, end=0) {
         var a = percent / 100,
         b = (end - start) * a,
@@ -12,5 +6,8 @@ $(document).ready(function () {
         return 'hsl('+c+', 100%, 35%)';
     }
 
-    $('.priority').css({'color': hsl_col_perc(text)});
+    $(".priority").each(function() {
+        var val = parseFloat(this.textContent);
+        this.css({'color': hsl_col_perc(val)})
+    });
 });
