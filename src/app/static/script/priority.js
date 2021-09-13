058f7f8e0a0ef8d6e0b89ea6f78d1d7355d6643b
Guilherme de Lyra <guilyra12@gmail.com>
2021-09-13 18:49:16 -0300

    restyling priority /2

--- src/app/static/script/priority.js
+++ src/app/static/script/priority.js
@@ -2,13 +2,15 @@ $(document).ready(function () {
     // example: https://getbootstrap.com/docs/4.2/components/modal/
     // show modal
     var text = parseFloat(document.querySelector('.priority').textContent);
-
+    console.log(text);
+    console.log(hsl_col_perc(text));
+    
     function hsl_col_perc(percent, start=240, end=0) {
         var a = percent / 100,
         b = (end - start) * a,
         c = b + start;
         return 'hsl('+c+', 100%, 50%)';
-    };
+    }
 
     $('.priority').css({'color': hsl_col_perc(text)});
 });
