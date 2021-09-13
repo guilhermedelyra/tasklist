44a8de7a59947cb7dcf7b6db38049ad46a43453b
Guilherme de Lyra <guilyra12@gmail.com>
2021-09-13 00:44:59 -0300

    trying to fix static files serving /4

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,8 +2,7 @@ import os
 import sqlalchemy
 from flask import Flask
 from dotenv import load_dotenv
-from flask_talisman import Talisman
-from whitenoise import WhiteNoise
+# from flask_talisman import Talisman
 
 load_dotenv()
 
@@ -16,8 +15,8 @@ port = os.environ['POSTGRES_PORT']
 DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{_database}'
 
 app = Flask(__name__)
-Talisman(app)
-# app.wsgi_app = WhiteNoise(app.wsgi_app, root='src/app/static/')
+# Talisman(app)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.environ.get("SECRET_KEY")
 app.app_context().push()
